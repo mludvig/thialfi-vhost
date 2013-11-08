481b635c4eb98d6123f0bf69d042836750691f9d
Michal Ludvig <michal.ludvig@sms-01.aws.e-it.co.nz>
2013-11-08 03:54:10 +0000

    Improved setup to handle system python modules

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ set -x -e
 
 source setup-conf.sh
 
+fatal() {
+	echo -e "\e[31;1m$1\e[0m"
+	exit 1
+}
+
 if [ "${PROJECT_REPO}x" = "x" ]; then
 	PROJECT_REPO="$(dirname $(git config --get remote.origin.url))/${PROJECT}.git"
 fi
@@ -26,13 +31,24 @@ else
 	VIRTUALENV="/usr/bin/virtualenv"
 fi
 
-${VIRTUALENV} --clear .
+${VIRTUALENV} --clear --system-site-packages .
 . bin/activate
 python --version 2>&1 | awk 'NR==1{if ($2 < "2.6"){print "Python is tool old. Required 2.6 or newer."; exit(1);}}'
 
-pip install PIL
+for module in PIL MySQLdb suds; do
+	python -c "import ${module}" 2>&1 || fatal "Python module '${module}' not found. Please install and re-run $0"
+	echo -e "\e[1m* ${module} is available\e[0m"
+done
+
 pip install django==1.3.7
 
-git clone ${PROJECT_REPO}
+if [ ! -d ${PROJECT} ]; then
+	git clone ${PROJECT_REPO}
 
-test -x ${PROJECT}/setup.sh && (cd ${PROJECT}; ./setup.sh)
+	test -x ${PROJECT}/setup.sh && (cd ${PROJECT}; ./setup.sh)
+fi
+
+getent group vhost.${PROJECT} || echo -e "Please create system group \e[1mvhost.${PROJECT}\e[0m"
+getent passwd vhost.${PROJECT} || echo -e "Please create system user \e[1mvhost.${PROJECT}\e[0m as a member of group \e[1mvhost.${PROJECT}\e[0m"
+
+echo -e "\e[1mAll done!\e[0m"
